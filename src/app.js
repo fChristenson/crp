a42a866ede9e852a4081f109b3bee403ff291973
fChristenson <fredrik.christenson@gmail.com>
2020-03-28 06:12:28 +0100

    wp

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 
+const jsWaitTime = 0;
+const cssWaitTime = 0;
+
+app.get("/main.js", (req, res) => {
+  wait(jsWaitTime, () => {
+    res.sendFile(path.resolve(__dirname, "..", "main.js"));
+  })
+});
+
+app.get("/main.css", (req, res) => {
+  wait(cssWaitTime, () => {
+    res.sendFile(path.resolve(__dirname, "..", "main.css"));
+  })
+});
+
 app.get("/", (req, res) => {
-  res.end("foo");
+  res.sendFile(path.resolve(__dirname, "..", "index.html"));
 });
 
 module.exports = app;
+
+const wait = (timeout, fn) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve(fn());
+    }, timeout);
+  })
+};
